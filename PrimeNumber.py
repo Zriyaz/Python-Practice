65d1cd8b19b1bfc7d10cd4e98ee9475050c88091
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-20 07:17:56 +0000

    print(check_prime(n))

---
+++ PrimeNumber.py
@@ -0,0 +1,12 @@
+import math
+def check_prime(num):
+    if num == 1:
+        return "Not prime"
+    sq = int(math.sqrt(num))
+    for x in range(2, sq+1):
+        if num % x == 0:
+            return "Not prime"
+    return "Prime"
+for _ in range(int(input())):
+    n = int(input())
+    print(check_prime(n))
